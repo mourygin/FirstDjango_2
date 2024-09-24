541092514395a642becfbdeebd99071456818566
mourygin <55830119+mourygin@users.noreply.github.com>
2024-09-24 22:33:08 +0300

    Add files via upload

---
+++ example1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+class index2(TemplateView):
+    template_name = 'index2.html'
